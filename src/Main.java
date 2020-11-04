945aa01b49efcb0dc998b06a6106a68e8fd50102
HaydenDoty <hayden.doty@ou.edu>
2020-11-04 17:05:17 -0600

    Now it has answers to questions

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
 
 public class Main {
-
+/* 
+ * it took me awhile to remember the git things to connect the repo to my project so I will fist give an explanation
+ * 
+ * Hashmaps are good for when you have data that need s associated with each other, like a dictionary
+ * TreeMaps are similar, but are best for when your data must be sorted as the TreeMap automatically sorts the data
+ * Therefor, you should use TreeMap if you must guarantee the order of key,value pairs
+ */
 }
